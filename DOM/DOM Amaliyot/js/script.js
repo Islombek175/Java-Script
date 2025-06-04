a6b2941650f09fa8292fd5bb032dcdd35b7345dc
Islombek Matkarimov <matkaimovislambek@gmail.com>
2025-06-04 22:23:52 +0500

    DOM Amaliyot ishlandi

--- DOM/DOM Amaliyot/js/script.js
+++ DOM/DOM Amaliyot/js/script.js
@@ -1,23 +1,16 @@
-let removeButton = document.querySelector(".remove_button")
-let advTextelement = document.querySelector(".promo__adv-title")
-let ads = document.querySelectorAll(".adv")
-let promoGenreElement = document.querySelector(".promo__genre")
-let series = document.querySelectorAll(".promo__interactive-item")
+const advs = document.querySelector(".promo__adv"),
+	genre = document.querySelector(".promo__genre"),
+	bg = document.querySelector(".promo__bg");
 
-removeButton.addEventListener("click", function () {
-	ads.forEach(item => {
-		item.remove()
-	});
-	removeButton.remove();
-	advTextelement.remove()
-})
+const series = document.querySelectorAll(".promo__interactive-item")
+let allSeries = [];
 
-promoGenreElement.innerText = "KOMEDIYA"
+advs.remove()
+genre.textContent = "COMEDY"
+bg.style.backgroundImage = "url(./img/1.jpg)"
 
-document.querySelector(".promo__bg").style.background.url = "../img/1.jpg"
-
-function showSeries() {
-	series.forEach(item => {
-		
-	})
+for (let i = 0; i < 5; i++) {
+	allSeries.push(`${i+1}. ${series[i].textContent.trim()}`)
 }
+
+console.log(allSeries);
