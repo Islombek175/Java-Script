d1568a4937e5a0810776a6ba96e0b6b8561e6074
Islombek175 <matkaimovislambek@gmail.com>
2025-06-04 17:22:51 +0500

    amaliyot 4.0

--- SeriesDB Amaliyot/script.js
+++ SeriesDB Amaliyot/script.js
@@ -1,52 +1,76 @@
-const numberOfSeries = +prompt("Nechta seial ko'rdingiz?", "")
-
 let actors = {}
 let genres = []
 
 let seriesDB = {
-	count: numberOfSeries,
+	count: 0,
 	series: {},
 	actors: actors,
 	genres: genres,
 	private: false,
-}
-const watchedSeries = () => {
-	for(let i = 0; i < 2; i ++ ){
-		let a1 = prompt(`Ohirgi ko'rgan serialingiz? ${i+1}`, "");
-		let a2 = +prompt(`Necha baxo berasiz? ${i+1}`, "");
-	
-		if(a1 != null && a2 != null && a1 != "" && a2 != ""){
-		seriesDB.series[a1] = a2
+	start: function () {
+		seriesDB.count = +prompt("Nechta serial ko'rdingiz?", '')
+
+		while (
+			seriesDB.count == '' ||
+			seriesDB.count == null ||
+			isNaN(seriesDB.count)
+		) {
+			seriesDB.count = +prompt("Nechta serial ko'rdingiz?", '')
 		}
-		else{
+	},
+	setSeries: function () {
+		for (let i = 0; i < 2; i++) {
+			let a1 = prompt(`Ohirgi ko'rgan serialingiz? ${i + 1}`, '')
+			let a2 = +prompt(`Necha baxo berasiz? ${i + 1}`, '')
+		}
+
+		if (a1 != null && a2 != null && a1 != '' && a2 != '') {
+			seriesDB.series[a1] = a2
+		} else {
 			i--
 		}
-	}
+	},
+	detectingLevel: function () {
+		if (seriesDB.count < 5) {
+			console.log("Kam serial ko'ripsiz")
+		} else if (seriesDB.count >= 5 && seriesDB.count < 10) {
+			console.log('Siz classik tamoshabin ekansiz')
+		} else if (count >= 10) {
+			console.log('Siz serialchi zvezda ekansiz')
+		}
+	},
+	visibleDB: function () {
+		if (seriesDB.private == true) {
+			seriesDB.private = false
+		} else {
+			seriesDB.private = true
+		}
+	},
+	writeGenres: function () {
+		confirm('')
+	},
+	showDb: function () {
+		if (seriesDB.private == true) {
+			console.log(seriesDB)
+		} else if (seriesDB.private == false) {
+			console.log("Ma'lumot mahfiy saqlanmoqda")
+		}
+	},
+	writeGenres: function () {
+		for (let i = 0; i < 3; i++) {
+			let genres = prompt(`Yaxshi ko’rgan janringiz ${i + 1}`)
+			if (genres == "" || genres == null) {
+			i--
+			} else {
+				seriesDB.genres[i] = genres
+			}
+		}
+		seriesDB.genres.forEach(item => {
+			console.log(item);
+		})
+	},
 }
 
-const audience = () => {
-	if(seriesDB.count < 5){
-		console.log("Kam serial ko'ripsiz");
-	} else if( seriesDB.count >= 5 && seriesDB.count < 10){
-		console.log("Siz classik tamoshabin ekansiz");
-	} else if(count >= 10){
-		console.log("Siz serialchi zvezda ekansiz");
-	}
-}
+const movie = Object.create(seriesDB)
 
-const writeGenres = () => {
-	for(let i = 0; i < 3; i++){
-		let genres = prompt(`Yaxshi ko’rgan janringiz ${i+1}`)
-		seriesDB.genres[i] = genres
-	}
-}
-
-const showDB = () => {
-	if(seriesDB.private === false){
-		console.log(seriesDB);
-	} else if(seriesDB.private === true){
-		console.log("Sorry, we can't show you seriesDB.");
-	}
-}
-
-watchedSeries(), audience(), writeGenres(), showDB()
+console.log(movie);
