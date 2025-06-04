c6603343142d52f37db8d524c69f21c3c6dad71a
Islombek175 <matkaimovislambek@gmail.com>
2025-06-04 16:37:49 +0500

    amaliyot 3.0

--- SeriesDB Amaliyot/script.js
+++ SeriesDB Amaliyot/script.js
@@ -8,28 +8,45 @@ let seriesDB = {
 	series: {},
 	actors: actors,
 	genres: genres,
-	privat: false,
+	private: false,
 }
-
-for(let i = 0; i < 2; i ++ ){
-	let a1 = prompt(`Ohirgi ko'rgan serialingiz? ${i+1}`, "");
-	let a2 = +prompt(`Necha baxo berasiz? ${i+1}`, "");
-
-	if(a1 != null && a2 != null && a1 != "" && a2 != ""){
-	seriesDB.series[a1] = a2
+const watchedSeries = () => {
+	for(let i = 0; i < 2; i ++ ){
+		let a1 = prompt(`Ohirgi ko'rgan serialingiz? ${i+1}`, "");
+		let a2 = +prompt(`Necha baxo berasiz? ${i+1}`, "");
+	
+		if(a1 != null && a2 != null && a1 != "" && a2 != ""){
+		seriesDB.series[a1] = a2
+		}
+		else{
+			i--
+		}
 	}
-	else{
-		i --
+}
+
+const audience = () => {
+	if(seriesDB.count < 5){
+		console.log("Kam serial ko'ripsiz");
+	} else if( seriesDB.count >= 5 && seriesDB.count < 10){
+		console.log("Siz classik tamoshabin ekansiz");
+	} else if(count >= 10){
+		console.log("Siz serialchi zvezda ekansiz");
 	}
 }
 
-
-if(seriesDB.count < 5) {
-	console.log("Kam serial ko'ripsiz");
-} else if ( seriesDB.count >= 5 && seriesDB.count < 10) {
-	console.log("Siz classik tamoshabin ekansiz");
-} else if(count >= 10) {
-	console.log("Siz serialchi zvezda ekansiz");
+const writeGenres = () => {
+	for(let i = 0; i < 3; i++){
+		let genres = prompt(`Yaxshi ko’rgan janringiz ${i+1}`)
+		seriesDB.genres[i] = genres
+	}
 }
-console.log(seriesDB);
 
+const showDB = () => {
+	if(seriesDB.private === false){
+		console.log(seriesDB);
+	} else if(seriesDB.private === true){
+		console.log("Sorry, we can't show you seriesDB.");
+	}
+}
+
+watchedSeries(), audience(), writeGenres(), showDB()
