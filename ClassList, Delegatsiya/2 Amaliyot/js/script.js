0e810e4eba3c5f8db6ef0ebfcf91632dd839fbde
Islombek Matkarimov <matkaimovislambek@gmail.com>
2025-06-13 22:40:04 +0500

    Amaliyot bajarildi

--- ClassList, Delegatsiya/2 Amaliyot/js/script.js
+++ ClassList, Delegatsiya/2 Amaliyot/js/script.js
@@ -1,24 +1,34 @@
 window.addEventListener("DOMContentLoaded", () => {
 	const tabs = document.querySelectorAll(".tabheader__item"),
 		tabContents = document.querySelectorAll(".tab_content"),
-		tabParents = document.querySelectorAll(".tabheader__items")
+		tabsParent = document.querySelector(".tabheader__items")
 
-	function hiddenTabContents() {
+	function hideTabContents() {
 		tabContents.forEach(tabContent => {
 			tabContent.style.display = "none"
-		})
-
+		})		
 		tabs.forEach(tab => {
 			tab.classList.remove("tabheader__item_active")
 		})
 	}
-
-	function showTabContent(index) {
-		tabContents[index].style.display = "block"
-		tabs[index].style.add("tabheader__item_active")
+	
+	function showTabConten(index) {
+		tabs[index].classList.add("tabheader__item_active")
+		tabContents[index].style.display = "flex"
 	}
 
-	hiddenTabContents()
-	showTabContent(0)
+	hideTabContents()
+
+	tabsParent.addEventListener("click", event => {
+		const target = event.target
+		if(target && target.classList.contains("tabheader__item")) {
+			tabs.forEach((tab, index) => {
+				if(target === tab) {
+					hideTabContents()
+					showTabConten(index)
+				}
+			})
+		}
+	})
 })
 
